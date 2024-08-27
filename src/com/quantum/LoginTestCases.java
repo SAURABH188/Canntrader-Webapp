dff0bb24a151bcb9e8e9b25a4f22b27da9749153
SAURABH188 <saurabhkumbhar187@gmail.com>
2024-08-27 18:28:19 +0530

    Test

--- src/com/quantum/LoginTestCases.java
+++ src/com/quantum/LoginTestCases.java
@@ -3,23 +3,12 @@ package com.quantum;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.AssertJUnit;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.AssertJUnit;
-import static org.testng.Assert.assertTrue;
-
 import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class LoginTestCases {
 	 private WebDriver driver;
